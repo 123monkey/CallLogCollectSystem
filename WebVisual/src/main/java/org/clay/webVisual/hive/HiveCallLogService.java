76043fa0b2f8cbb61cd75fa5698d84110997b29a
clay4 <clay4444@126.com>
2017-12-02 22:23:42 +0800

    修改SparkSql集成Hive的代码，使用SparkSql分布式查询引擎ThriftServer的方式集成，并通过C3P0池化

--- WebVisual/src/main/java/org/clay/webVisual/hive/HiveCallLogService.java
+++ WebVisual/src/main/java/org/clay/webVisual/hive/HiveCallLogService.java
@@ -1,19 +1,26 @@
 package org.clay.webVisual.hive;
 
 import org.apache.hadoop.hbase.client.Result;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
+//import org.apache.spark.SparkConf;
+//import org.apache.spark.SparkContext;
+//import org.apache.spark.api.java.function.ForeachFunction;
+//import org.apache.spark.sql.Dataset;
+//import org.apache.spark.sql.Row;
+//import org.apache.spark.sql.SparkSession;
 import org.clay.webVisual.domain.CallLog;
 import org.clay.webVisual.domain.CallLogStat;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -21,8 +28,11 @@ import java.util.List;
 @Service("hiveCallLogService")
 public class HiveCallLogService {
 
+    @Resource(name="sparkSQLDataSource")
+    private DataSource ds ;
+
     //hiveserver2连接串
-    private static String url = "jdbc:hive2://mini1:10000/mydb" ;
+    private static String url = "jdbc:hive2://mini1:10000/" ;
     //驱动程序类
     private static String driverClass = "org.apache.hive.jdbc.HiveDriver" ;
 
@@ -37,25 +47,24 @@ public class HiveCallLogService {
      * 查询最近的通话记录,使用hive进行mr查询.
      */
     public CallLog findLatestCallLog(String phoneNum){
-        try {
-            Connection conn = DriverManager.getConnection(url);
-            System.out.println(conn);
-            Statement st = conn.createStatement();
-            String sql = "select * from ext_calllogs_in_hbase where id like '%"+ phoneNum+"%' order by callTime desc limit 1" ;
-            ResultSet rs = st.executeQuery(sql);
-            CallLog log = null ;
-            if(rs.next()){
-                log = new CallLog();
-                log.setCaller(rs.getString("caller"));
-                log.setCallee(rs.getString("callee"));
-                log.setCallTime(rs.getString("callTime"));
-                log.setCallDuration(rs.getString("callDuration"));
-            }
-            rs.close();
-            return log ;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//        try {
+//            Connection conn = DriverManager.getConnection(url);
+//            Statement st = conn.createStatement();
+//            String sql = "select * from ext_calllogs_in_hbase where id like '%"+ phoneNum+"%' order by callTime desc limit 1" ;
+//            ResultSet rs = st.executeQuery(sql);
+//            CallLog log = null;
+//            if (rs.next()) {
+//                log = new CallLog();
+//                log.setCaller(rs.getString("caller"));
+//                log.setCallee(rs.getString("caller"));
+//                log.setCallTime(rs.getString("callTime"));
+//                log.setCallDuration(rs.getString("callDuration"));
+//            }
+//            rs.close();
+//            return log;
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
         return null ;
     }
 
@@ -63,17 +72,42 @@ public class HiveCallLogService {
      * 查询指定人员指定年份中各个月份的通话次数
      */
     public List<CallLogStat> statCallLogsCount(String caller, String year){
-        SparkSession sess = SparkSession.builder().appName("SparkHive").enableHiveSupport().master("spark://s201:7077").getOrCreate();
-        String sql = "select count(*) ,substr(calltime,1,6) from ext_calllogs_in_hbase " +
-                "where caller = '" + caller + "' and substr(calltime,1,4) == '" + year
-                + "' group by substr(calltime,1,6) order by substr(calltime,1,6) asc";
-        Dataset<Row> df = sess.sql(sql);
-        List<Row> rows = df.collectAsList();
-        List<CallLogStat> list = new ArrayList<CallLogStat>();
-        for(Row row : rows){
-            list.add(new CallLogStat(row.getString(2),row.getInt(1)));
+//        SparkSession sess = SparkSession.builder().appName("SparkHive").enableHiveSupport().master("spark://s201:7077").getOrCreate();
+//        String sql = "select count(*) ,substr(calltime,1,6) from ext_calllogs_in_hbase " +
+//                "where caller = '" + caller + "' and substr(calltime,1,4) == '" + year
+//                + "' group by substr(calltime,1,6) order by substr(calltime,1,6) asc";
+//        Dataset<Row> df = sess.sql(sql);
+//        List<Row> rows = df.collectAsList();
+//        List<CallLogStat> list = new ArrayList<CallLogStat>();
+//        for(Row row : rows){
+//            list.add(new CallLogStat(row.getString(2),row.getInt(1)));
+//        }
+//        return list;
+        try {
+//            Class.forName("org.apache.hive.jdbc.HiveDriver");
+//            Connection conn = DriverManager.getConnection("jdbc:hive2://s201:10000");
+            Connection conn = ds.getConnection();
+            String sql = "select count(*) ,substr(calltime,1,6) from mydb.ext_calllogs_in_hbase " +
+                    "where caller = '" + caller + "' and substr(calltime,1,4) == '" + year
+                    + "' group by substr(calltime,1,6) order by substr(calltime,1,6) desc";
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+
+            List<CallLogStat> list = new ArrayList<CallLogStat>();
+            while (rs.next()) {
+                long count = rs.getLong(1);
+                String ym = rs.getString(2);
+                list.add(new CallLogStat(ym, (int)count));
+            }
+            rs.close();
+            st.close();
+            conn.close();
+            return list ;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return list;
+
+        return null ;
     }
 
     /**
--- WebVisual/src/test/java/org/clay/webVisual/test/TestSpark.java
+++ WebVisual/src/test/java/org/clay/webVisual/test/TestSpark.java
@@ -1,13 +1,17 @@
 package org.clay.webVisual.test;
 
-import org.apache.spark.SparkConf;
-import org.apache.spark.SparkContext;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
-import org.clay.webVisual.domain.CallLogStat;
+//import org.apache.spark.SparkConf;
+//import org.apache.spark.SparkContext;
+//import org.apache.spark.sql.Dataset;
+//import org.apache.spark.sql.Row;
+//import org.apache.spark.sql.SparkSession;
+//import org.clay.webVisual.domain.CallLogStat;
 import org.junit.Test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,18 +21,44 @@ import java.util.List;
 public class TestSpark {
     @Test
     public void test1(){
+//        String caller = "13341109505" ;
+//        String year = "2017" ;
+//        SparkSession sess = SparkSession.builder().enableHiveSupport().appName("SparkHive").master("spark://s201:7077").getOrCreate();
+//        String sql = "select count(*) ,substr(calltime,1,6) from ext_calllogs_in_hbase " +
+//                "where caller = '" + caller + "' and substr(calltime,1,4) == '" + year
+//                + "' group by substr(calltime,1,6) order by substr(calltime,1,6)";
+//        Dataset<Row> df = sess.sql(sql);
+//        List<Row> rows = df.collectAsList();
+//        List<CallLogStat> list = new ArrayList<CallLogStat>();
+//        for (Row row : rows) {
+//            System.out.println(row.getString(1));
+//            list.add(new CallLogStat(row.getString(1), (int)row.getLong(0)));
+//        }
+    }
+
+    /**
+     *  使用thrift 服务的话，就要在每台spark的worker节点上配置Hive
+     * @throws Exception
+     */
+    @Test
+    public void testSparkSQLByThriftServer() throws Exception {
+        Class.forName("org.apache.hive.jdbc.HiveDriver");
+        Connection conn = DriverManager.getConnection("jdbc:hive2://mini1:10000");
+
         String caller = "13341109505" ;
         String year = "2017" ;
-        SparkSession sess = SparkSession.builder().enableHiveSupport().appName("SparkHive").master("spark://s201:7077").getOrCreate();
-        String sql = "select count(*) ,substr(calltime,1,6) from ext_calllogs_in_hbase " +
+        String sql = "select count(*) ,substr(calltime,1,6) from mydb.ext_calllogs_in_hbase " +
                 "where caller = '" + caller + "' and substr(calltime,1,4) == '" + year
-                + "' group by substr(calltime,1,6) order by substr(calltime,1,6)";
-        Dataset<Row> df = sess.sql(sql);
-        List<Row> rows = df.collectAsList();
-        List<CallLogStat> list = new ArrayList<CallLogStat>();
-        for (Row row : rows) {
-            System.out.println(row.getString(1));
-            list.add(new CallLogStat(row.getString(1), (int)row.getLong(0)));
+                + "' group by substr(calltime,1,6) order by substr(calltime,1,6) desc";
+        Statement st = conn.createStatement();
+        ResultSet rs = st.executeQuery(sql);
+        while(rs.next()){
+            long count  = rs.getLong(1);
+            String ym  = rs.getString(2);
+            System.out.println(ym + " : " + count);
         }
+        rs.close();
+        st.close();
+        conn.close();
     }
 }
